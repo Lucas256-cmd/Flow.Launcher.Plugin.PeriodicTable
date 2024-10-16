7cbe1e0986675915354ec7c68981324a46a2ad5b
Lucas <lucasmonteyne@gmail.com>
2024-10-16 23:18:27 +0200

    maybe added an easteregg...

--- plugin/periodicTable.py
+++ plugin/periodicTable.py
@@ -3,6 +3,32 @@ import periodictable
 
 class Main(FlowLauncher):
     def query(self, param: str = '') -> list:
+        # easteregg
+        if param == "Blobfish":
+            return [
+                {
+                    "title": "OOPS THIS IS NOT A PERIODIC ELEMENT",
+                    "subTitle": "Blobfish is a deep sea fish that inhabits the deep waters off the coasts of Australia and Tasmania.",
+                    "icoPath": "Images/app.png",
+                    "score": 0
+                },
+                {
+                    "title": "info",
+                    "subTitle": "https://www.wikiwand.com/en/articles/Psychrolutidae",
+                    "jsonRPCAction": {
+                        "method": "open_url",
+                        "parameters": ["https://www.wikiwand.com/en/articles/Psychrolutidae"]
+                    },
+                    "icoPath": "Images/app.png",
+                    "score": 0
+                },
+                {
+                    "title": "If you want to see the full message use ctrl + ]",
+                    "subTitle": "And ctrl + [ to set it back to normal",
+                    "icoPath": "Images/app.png",
+                    "score": 0
+                }
+            ]
         try:
             param_element = periodictable.elements.symbol(param)
             atomic_number = str(param_element.number)
