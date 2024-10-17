15d94a54b1dd0d7b5000c7aaabdacf605d76617f
Lucas <lucasmonteyne@gmail.com>
2024-10-17 15:41:37 +0200

    Polishing of the results

--- plugin/periodicTable.py
+++ plugin/periodicTable.py
@@ -2,38 +2,44 @@ from flowlauncher import FlowLauncher
 import webbrowser
 import JSONLookup
 
+
+def open_url(url):
+    webbrowser.open(url)
+
+
 class Main(FlowLauncher):
     def query(self, param: str = '') -> list:
         # easteregg
         if param.lower() == "blobfish":
             return [
                 {
-                    "title": "OOPS THIS IS NOT A PERIODIC ELEMENT",
+                    "title": "OOPS this is not an element",
                     "subTitle": "The Blobfish is a deep sea fish that inhabits the deep waters of the Atlantic and Pacific Ocean. The blobfish has a short, broad tongue and conical teeth that are slightly recurved and are arranged in bands in irregular rows along the premaxillaries (whatever this word may mean).",
                     "icoPath": "Images/b.png",
-                    "score": 4
+                    "score": 4,
+                    "ContextData": {"title": "Blobfish"}
                 },
                 {
-                    "title": "I don't even know why you searched for this",
+                    "title": "I don't even know why you searched for this.",
                     "subTitle": "But here is a picture of a Blobfish",
                     "icoPath": "Images/b.png",
                     "score": 3
                 },
                 {
-                    "title": "If you want to see the full message of the first result use ctrl + ]",
-                    "subTitle": "And ctrl + [ to set it back to normal",
+                    "title": "If you want to see the full blobfish info use ctrl + ]",
+                    "subTitle": "And ctrl + [ to set it back to normal, If the blobfish info is still too long go to the context menu of the first result.",
                     "icoPath": "Images/b.png",
                     "score": 2
                 },
                 {
-                    "title": "more info",
+                    "title": "More info",
                     "subTitle": "https://en.wikipedia.org/wiki/Psychrolutidae",
                     "jsonRPCAction": {
                         "method": "open_url",
                         "parameters": ["https://en.wikipedia.org/wiki/Psychrolutidae"]
                     },
                     "icoPath": "Images/b.png",
-                    "score": 1
+                    "score": 0
                 },
             ]
         try:
@@ -47,7 +53,7 @@ class Main(FlowLauncher):
                     "title": param_element.name.capitalize(),
                     "subTitle": param_element.symbol,
                     "icoPath": "Images/app.png",
-                    "score": 2
+                    "score": 2,
                 },
                 {
                     "title": "Atomic Number: " + atomic_number,
@@ -66,11 +72,36 @@ class Main(FlowLauncher):
             result = [
                 {
                     "title": "Element not found",
+                    "subTitle": "This plugin has no autocomplete feature (yet), you have to type the FULL name or symbol of the element.",
                     "icoPath": "Images/app.png",
-                    "score": 0
+                    "score": 0,
+                    "ContextData": {"title": "Element not found"}
                 }
             ]
         return result
 
-    def open_url(self, url):
-        webbrowser.open(url)
+    def context_menu(self, data):
+        if data['title'] == "Element not found":
+            return [
+                {
+                    "title": "This plugin has no autocomplete feature (yet).",
+                    "subTitle": "You have to type the FULL name or symbol of the element.",
+                    "score": 0,
+                    "icoPath": "Images/app.png",
+                }
+            ]
+        elif data['title'] == "Blobfish":
+            return [
+                {
+                    "title": "The Blobfish is a deep sea fish that inhabits the deep waters of the Atlantic and Pacific Ocean.",
+                    "subTitle": "The blobfish has a short, broad tongue and conical teeth that are slightly recurved and are arranged in bands in irregular rows along the premaxillaries (whatever this word may mean).",
+                    "icoPath": "Images/b.png",
+                    "score": 1
+                },
+                {
+                    "title": "If the blobfish info is still too long,",
+                    "subTitle": "try it making flow wider again now.",
+                    "icoPath": "Images/b.png",
+                    "score": 0
+                }
+            ]
