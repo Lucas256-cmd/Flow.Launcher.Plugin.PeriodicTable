f36addb927a6af3979d3299eb21c0ae4bff33cd6
Lucas <lucasmonteyne@gmail.com>
2024-10-17 08:03:15 +0200

    updated scores to keep order in returned elements

--- plugin/periodicTable.py
+++ plugin/periodicTable.py
@@ -11,18 +11,19 @@ class Main(FlowLauncher):
                     "title": "OOPS THIS IS NOT A PERIODIC ELEMENT",
                     "subTitle": "The Blobfish is a deep sea fish that inhabits the deep waters of the Atlantic and Pacific Ocean. The blobfish has a short, broad tongue and conical teeth that are slightly recurved and are arranged in bands in irregular rows along the premaxillaries (whatever this word may mean).",
                     "icoPath": "Images/b.png",
-                    "score": 0
+                    "score": 4
                 },
                 {
                     "title": "I don't even know why you searched for this",
                     "subTitle": "But here is a picture of a Blobfish",
                     "icoPath": "Images/b.png",
+                    "score": 3
                 },
                 {
                     "title": "If you want to see the full message of the first result use ctrl + ]",
                     "subTitle": "And ctrl + [ to set it back to normal",
                     "icoPath": "Images/b.png",
-                    "score": 0
+                    "score": 2
                 },
                 {
                     "title": "more info",
@@ -32,7 +33,7 @@ class Main(FlowLauncher):
                         "parameters": ["https://en.wikipedia.org/wiki/Psychrolutidae"]
                     },
                     "icoPath": "Images/b.png",
-                    "score": 0
+                    "score": 1
                 },
             ]
         try:
@@ -44,13 +45,13 @@ class Main(FlowLauncher):
                     "title": param_element.name,
                     "subTitle": param_element.symbol,
                     "icoPath": "Images/app.png",
-                    "score": 0
+                    "score": 2
                 },
                 {
                     "title": "Atomic Number: " + atomic_number,
                     "subTitle": "Atomic Mass: " + atomic_weight,
                     "icoPath": "Images/app.png",
-                    "score": 0
+                    "score": 1
                 },
             ]
         except ValueError:
