51cc355cddf03634abfdd1a7f6e7e57e7cb57bfe
Lucas <lucasmonteyne@gmail.com>
2024-10-17 17:08:51 +0200

    added search completion

--- plugin/JSONLookup.py
+++ plugin/JSONLookup.py
@@ -27,7 +27,7 @@ class Element:
         self.name = get_element_name_by_symbol(element_name_or_symbol)
         self.symbol = get_element_symbol_by_name(element_name_or_symbol)
         if self.name is None and self.symbol is None:
-            raise ValueError("Element not found")
+            self.partial_match(element_name_or_symbol)
         if self.name is None:
             # This means that the user entered the element name
             self.name = element_name_or_symbol.lower()
@@ -35,17 +35,37 @@ class Element:
             # This means that the user entered the element symbol
             self.symbol = element_name_or_symbol.capitalize()
 
+        if self.name:
+            self.element_dict = get_element_dict(self.name)
+            self.name = self.name.capitalize()  # Capitalization done after name and symbol are set because it doesn't have to be lowercase anymore because there is no need to search in the dictionary anymor
+            self.atomic_number = str(self.element_dict['number']) if self.element_dict[
+                'number'] else "Unknown or not in database"
+            self.atomic_mass = str(self.element_dict['atomic_mass']) if self.element_dict[
+                'atomic_mass'] else "Unknown or not in database"
+            self.boiling_point = (str(self.element_dict['boil']) + " K") if self.element_dict[
+                'boil'] else "Unknown or not in database"
+            self.melting_point = (str(self.element_dict['melt']) + " K") if self.element_dict[
+                'melt'] else "Unknown or not in database"
+            self.discoverer = self.element_dict['discovered_by'] if self.element_dict[
+                'discovered_by'] else "Discovered by unknown or not in database"
+            self.named_by = self.element_dict['named_by'] if self.element_dict[
+                'named_by'] else "Named by unknown or not in database"
 
-        self.element_dict = get_element_dict(self.name)
-        self.atomic_number = str(self.element_dict['number']) if self.element_dict[
-            'number'] else "Unknown or not in database"
-        self.atomic_mass = str(self.element_dict['atomic_mass']) if self.element_dict[
-            'atomic_mass'] else "Unknown or not in database"
-        self.boiling_point = (str(self.element_dict['boil']) + " K") if self.element_dict[
-            'boil'] else "Unknown or not in database"
-        self.melting_point = (str(self.element_dict['melt']) + " K") if self.element_dict[
-            'melt'] else "Unknown or not in database"
-        self.discoverer = self.element_dict['discovered_by'] if self.element_dict[
-            'discovered_by'] else "Discovered by unknown or not in database"
-        self.named_by = self.element_dict['named_by'] if self.element_dict[
-            'named_by'] else "Named by unknown or not in database"
+    def partial_match(self, element_name_or_symbol):
+        # only run this if element_name_or_symbol is not "" else every element will be matched and the last element will be returned
+        if element_name_or_symbol != "":
+            for name, element in periodic_table.items():
+                if name != "order" and (element_name_or_symbol.lower() in name):
+                    self.name = name
+                    self.symbol = element['symbol']
+                    break
+            if self.name is None:
+                for name, element in periodic_table.items():
+                    if name != "order" and (element_name_or_symbol.lower() in element['symbol']):
+                        self.name = name
+                        self.symbol = element['symbol']
+                        break
+            if self.name is None:
+                raise ValueError("Element not found")
+        else:
+            raise ValueError("No element entered")
